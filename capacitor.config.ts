9f59ae87219badf83791b41ef3b0bb87062a3eb9
“JavierEM” <jembfh@gmail.com>
2026-07-20 12:23:19 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'clientsApp_React_Mongo',
+  webDir: 'dist'
+};
+
+export default config;
